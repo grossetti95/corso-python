87aaee4cf7446f785de7dac6c872bf0be13dbd66
grossetti95 <devrossettig@gmail.com>
2026-06-02 21:48:24 +0200

    indovina il frocio

--- lezione09/manip_list.py
+++ lezione09/manip_list.py
@@ -1,10 +1,42 @@
 def manipola_lista():
+    
     # Lista di 15 numeri da manipolare
     numeri = list(range(1, 16))
     print(f"Lista originale: {numeri}\n")
+    
     # 1: Estrazione con slicing
     # a) Primi 5 elementi
     primi_5 = numeri[:5]
     # b) Ultimi 5 elementi
     ultimi_5 = numeri[-5:]
+    # c) Elementi dal 6° al 10°
+    centrali = numeri[5:10]
+    # d) Elementi in posizione dispari
+    dispari = numeri[::2]
+    # e) Elementi in posizione pari
+    pari = numeri[1::2]
+    # f) Lista al contrario
+    al_contrario = numeri[::-1]
     
+    print(f"Primi 5 elementi: {primi_5}")
+    print(f"Ultimi 5 elementi: {ultimi_5}")
+    print(f"Elementi dal 6° al 10°: {centrali}")
+    print(f"Elementi in posizione dispari: {dispari}")
+    print(f"Elementi in posizione pari: {pari}")
+    print(f"Lista al contrario: {al_contrario}\n")
+    
+    #2: Modifica della lista
+    # a) crea una copia della lista originale
+    copia = numeri[:]
+    # b) modifica il primo elemento con 99
+    copia[0] = 99
+    # c) modifica l'ultimo elemento con 0
+    copia[-1] = 0
+    # d) inserisci il valore 50 a metà della lista (indice 7)
+    copia.insert(7, 50)
+    # e) rimuove il valore 8 della lista
+    copia.remove(8)
+    
+    
+if __name__ == "__main__":
+    manipola_lista()    
---
+++ programma-validissimo/prog_vali.py
@@ -0,0 +1,26 @@
+# Dati in un array e input per indovinare
+
+def indovina_il_frocio():
+    # Array di froci
+    froci = ["jenk", "chelaudio", "tisoni", "tipeppe", "phil", "mrmr"]
+    # Input dell'utente
+    indovina = input("Indovina il frocio: ")
+    
+    # Rendo input case-insensitive
+    indovina = indovina.lower()
+    
+    # Limite a 3 tentativi
+    tentativi = 2
+    while tentativi > 0:
+        if indovina in froci:
+            print("Validi!")
+            break
+        else:
+            tentativi-=1
+            print(f"Non è un frocio valido. Ti rimangono {tentativi} tentativi.")
+            indovina = input("Indovina il frocio: ")
+    if tentativi == 0:
+        print("Sei un ricchione di merda, hai perso!")
+        
+if __name__ == "__main__":
+    indovina_il_frocio()
