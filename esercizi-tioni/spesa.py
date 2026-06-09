f6f14066ba1acfd1a4db336fef6684569437253d
grossetti95 <devrossettig@gmail.com>
2026-06-09 10:58:36 +0200

    sistemato subtotale

--- esercizi-tioni/spesa.py
+++ esercizi-tioni/spesa.py
@@ -1,40 +1,43 @@
 # Genera un dizionario di prodotti
 lista_spesa = []
-prodotto = {}
+subtotale = 0.0 # Inizializzo subtotale prima del ciclo
+
 
 # Dichiaro gli input delle info prodotto
 while True:
-    
-    nome = input("Inserisci il nome: \n")
+    prodotto = {}
+    nome = str(input("Inserisci il nome: \n"))
     quantita = int(input("Quantità acquistata: \n"))
     prezzo = float(input("Prezzo unitario (prezzo con due decimali): \n"))
     
+    # Inserisco gli input come chiavi nel dizionario del prodotto
     prodotto["Nome"] = nome
     prodotto["Quantità"] = quantita
     prodotto["Prezzo"] = prezzo
     
-
     # Dichiaro variabile totale per stampare totale prezzo prodotto
     totale = quantita * prezzo
     prodotto["Totale"] = totale
-    
-    nuovo_prodotto = input("Vuoi inserire altro? s/n \n")
-    
+
+    # Subtotale
+    subtotale += totale
+
     # Append nella lista della spesa
     lista_spesa.append(prodotto)
     
-    # Subtotale
-    subtotale = []
-    subtotale.append(totale)
-      
-    # Condizione per uscire dal ciclo
+    # Ciclo indentato con condizione per uscire dal ciclo o renserimento altri prodotti
+    while True:
+        nuovo_prodotto = input("Vuoi inserire altro? s/n \n").lower()
+        if nuovo_prodotto == "s" or nuovo_prodotto == "n":
+            break
+        else:
+            print("Inserisci un carattere valido (s/n)")
+
+
     if nuovo_prodotto == "n":
         break
-    
-# Somma degli indici di subtotale
-subtot = sum(subtotale)
 
-
-print("Ecco la lista della spesa")
+# Print finale di tutto il report
+print("=== LISTA DELLA SPESA ===")
 print(lista_spesa)
-print(subtot)
+print(f"Il totale della spesa è {subtotale:.2f}")
